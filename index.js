4d55254b3fcc62f2e6d059bca8ca6afe83a34d7e
dev-Shivam999 <shivamswami982@gmail.com>
2024-10-17 17:23:47 +0530

    send

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.get('/lo', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3100;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
